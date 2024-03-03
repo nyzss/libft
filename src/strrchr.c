fe476d49709450110f1db9527c87f03f251c5301
Okan Emre Koca <oknakoca@gmail.com>
2024-03-03 21:48:45 +0100

    strrchr

--- src/strrchr.c
+++ src/strrchr.c
@@ -1,35 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memmove.c                                          :+:      :+:    :+:   */
+/*   strrchr.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: okoca <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 15:03:36 by okoca             #+#    #+#             */
 /*   Updated: 2024/02/08 15:11:58 by okoca            ###   ########.fr       */
-/*                                                                  =         */
+/*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dest, const void *src, size_t n)
+char	*ft_strrchr(const char *str, int c)
 {
-	unsigned char	*dst_c;
-	unsigned char	*src_c;
-	size_t			i;
+	int		i;
+	char	*occ;
 
-	if (!dest && !src)
-		return (NULL);
 	i = 0;
-	dst_c = (unsigned char *)dest;
-	src_c = (unsigned char *)src;
-	while (i < n)
+	occ = NULL;
+	while (str[i])
 	{
-		if (src_c < dst_c && dst_c < src_c + n)
-			dst_c[n - i] = src_c[n - i];
-		else
-			dst_c[i] = src_c[i];
+		if (str[i] == c)
+			occ = (char *)&str[i];
 		i++;
 	}
-	return (dest);
+	if (str[i] == c)
+		occ = (char *)&str[i];
+	return (occ);
 }
