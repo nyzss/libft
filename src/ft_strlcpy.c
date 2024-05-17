19034e8b192080cea3294893e945cf85fbe472bb
Okan Emre KOCA <oknakoca@gmail.com>
2024-05-17 10:47:06 +0200

    strlcpy

--- src/ft_strlcpy.c
+++ src/ft_strlcpy.c
@@ -27,3 +27,30 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	dst[i] = '\0';
 	return (ft_strlen(src));
 }
+
+/*
+#include <stdio.h>
+#include <bsd/string.h>
+
+int main()
+{
+	char *dest = malloc(sizeof(char) * 100);
+	char *real_dest = malloc(sizeof(char) * 100);
+
+	// mod
+
+	const char *value;
+	value = "lorem ipsum dolor sit amet";
+
+	size_t size;
+	size = 0;
+
+	// mod
+
+	size_t val = ft_strlcpy(dest, value, size);
+	size_t real_val = strlcpy(real_dest, value, size);
+
+	printf("dest: %s\nreal_dest: %s\n", dest, real_dest);
+	printf("val: %d\nreal val: %d\n", (int)val, (int)real_val);
+}
+*/
