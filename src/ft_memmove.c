1d99d144cf9361f92ae60b80362b29dc0f08d670
Okan Emre KOCA <oknakoca@gmail.com>
2024-05-17 15:07:07 +0200

    memmove testing

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -14,30 +14,34 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*dst_c;
-	unsigned char	*src_c;
+	unsigned char	*destination;
+	unsigned char	*source;
 	size_t			i;
 
+	i = 0;
 	if (!dest && !src)
 		return (NULL);
-	i = 0;
-	dst_c = (unsigned char *)dest;
-	src_c = (unsigned char *)src;
+	destination = (unsigned char *)dest;
+	source = (unsigned char *)src;
 	while (i < n)
 	{
-		if (src_c < dst_c && dst_c < src_c + n)
-			dst_c[n - i] = src_c[n - i];
+		if (source < destination && destination < source + n)
+			destination[n - i] = source[n - i];
 		else
-			dst_c[i] = src_c[i];
+			destination[i] = source[i];
 		i++;
 	}
 	return (dest);
 }
 
-// int main()
-// {
-
-// }
+int main()
+{
+	char sResult[] = {67, 68, 67, 68, 69, 0, 45};
+	char sResult2[] = {67, 67, 68, 68, 69, 0, 45};
+	void *ptr = ft_memmove(sResult + 1, sResult, 2);
+	 printf("%d\n", ptr == sResult + 1);
+	  //&& !memcmp(sResult, sResult2, 7))
+}
 
 /*
 	//4 
@@ -45,4 +49,37 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	char sResult[] = {67, 68, 67, 68, 69, 0, 45};
 	char sResult2[] = {67, 67, 68, 68, 69, 0, 45};
+*/
+
+
+/*
+void    *ft_memmove(void *dest, const void *src, size_t n)
+{
+        size_t                  i;
+        const char      *from;
+        char                    *to;
+
+        from = src;
+        to = dest;
+        if (to > from)
+        {
+                i = n;
+                while (i > 0)
+                {
+                        to[i - 1] = from[i - 1];
+                        i--;
+                }
+                to[i] = from[i];
+        }
+        else
+        {
+                i = 0;
+                while (i < n)
+                {
+                        to[i] = from[i];
+                        i++;
+                }
+        }
+        return (dest);
+}
 */
