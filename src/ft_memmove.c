e0f000739a142b65639835333d69e38c1bbd729c
nyzss <oknakoca@gmail.com>
2024-05-18 20:09:00 +0200

    testing memmove to find out whats wrong

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -12,11 +12,11 @@
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*destination;
-	unsigned char	*source;
-	size_t			i;
+	unsigned char *destination;
+	unsigned char *source;
+	size_t i;
 
 	i = 0;
 	if (!dest && !src)
@@ -34,52 +34,74 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
+#include <stdio.h>
+#include <string.h>
+
 int main()
 {
 	char sResult[] = {67, 68, 67, 68, 69, 0, 45};
-	char sResult2[] = {67, 67, 68, 68, 69, 0, 45};
-	void *ptr = ft_memmove(sResult + 1, sResult, 2);
-	 printf("%d\n", ptr == sResult + 1);
-	  //&& !memcmp(sResult, sResult2, 7))
+	char real[] = {67, 68, 67, 68, 69, 0, 45};
+
+	//char result_we_want[] = {67, 67, 68, 68, 69, 0, 45};
+
+	ft_memmove(sResult + 1, sResult, 2);
+	memmove(real + 1, sResult, 2);
+
+	int i = 0;
+	while (i < 7)
+	{
+		if (sResult[i] < 10 && real[i] < 10)
+			printf("0%d - 0%d", sResult[i], real[i]);
+		else {
+			printf("%d - %d", sResult[i], real[i]);
+		}
+		if (sResult[i] != real[i])
+			printf(" | NOT SAME\n");
+		else {
+			printf("\n");
+		}
+		i++;
+	}
+	// should return 1 if it works
+	printf("second check: %d\n", memcmp(sResult, real, 7));
 }
 
 /*
-	//4 
+	//4
 	 check(ft_memmove(sResult + 1, sResult, 2) == sResult + 1 && !memcmp(sResult, sResult2, 7)); showLeaks(); //reverse
 
 	char sResult[] = {67, 68, 67, 68, 69, 0, 45};
 	char sResult2[] = {67, 67, 68, 68, 69, 0, 45};
 */
 
-
 /*
 void    *ft_memmove(void *dest, const void *src, size_t n)
 {
-        size_t                  i;
-        const char      *from;
-        char                    *to;
+		size_t                  i;
+		const char      *from;
+		char                    *to;
 
-        from = src;
-        to = dest;
-        if (to > from)
-        {
-                i = n;
-                while (i > 0)
-                {
-                        to[i - 1] = from[i - 1];
-                        i--;
-                }
-                to[i] = from[i];
-        }
-        else
-        {
-                i = 0;
-                while (i < n)
-                {
-                        to[i] = from[i];
-                        i++;
-                }
-        }
-        return (dest);
+		from = src;
+		to = dest;
+		if (to > from)
+		{
+				i = n;
+				while (i > 0)
+				{
+						to[i - 1] = from[i - 1];
+						i--;
+				}
+				to[i] = from[i];
+		}
+		else
+		{
+				i = 0;
+				while (i < n)
+				{
+						to[i] = from[i];
+						i++;
+				}
+		}
+		return (dest);
 }
 */
