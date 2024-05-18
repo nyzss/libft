99c114d5e0fa0a814738ea2ed6c7af1293fa1ee8
nyzss <oknakoca@gmail.com>
2024-05-18 23:22:47 +0200

    fixed strnstr

--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strnstr.c                                          :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: okoca <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 15:03:36 by okoca             #+#    #+#             */
-/*   Updated: 2024/02/08 15:11:58 by okoca            ###   ########.fr       */
+/*   Updated: 2024/05/18 23:22:13 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (!needle)
-		return ((char *)haystack);
+	if (needle[0] == '\0')
+		return ((char*)haystack);
 	while (i < len && haystack[i])
 	{
 		j = 0;
 		if (needle[j] == haystack[i])
 		{
-			while (needle[j] && needle[j] == haystack[i + j])
+			while ((needle[j] && needle[j] == haystack[i + j]) && i + j < len)
 				j++;
 			if (needle[j] == '\0')
 				return ((char *)&haystack[i]);
@@ -34,3 +34,36 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
+
+
+  /*
+		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "", 10)))
+			ft_print_result("NULL");
+  */
+
+/*
+#include <stdio.h>
+#include <bsd/string.h>
+
+int main()
+{
+	// char haystack[30] = "aaabcabcd";
+	char haystack[30] = "lorem ipsum dolor sit amet";
+	// char needle[10] = "aabc";
+	char *empty = "";
+
+	char *real = strnstr(haystack, "", 10);
+	char *mine = ft_strnstr(haystack, "", 10);
+
+	printf("real: %s, result: %d\n", real, real == empty);
+	printf("mine: %s, result: %d\n", mine, mine == empty);
+}
+*/
+
+/*
+tests that are not passing:
+check(ft_strnstr(empty, "", -1) == empty); good
+check(ft_strnstr(empty, "", 0) == empty); good
+check(ft_strnstr(haystack, "cd", 8) == NULL);
+*/
+
