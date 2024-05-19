03d9413770542ea3c26cd90a7d3b43a3b3393018
nyzss <oknakoca@gmail.com>
2024-05-19 20:32:53 +0200

    fixed strlcat

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: okoca <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 15:03:36 by okoca             #+#    #+#             */
-/*   Updated: 2024/02/08 15:11:58 by okoca            ###   ########.fr       */
+/*   Updated: 2024/05/19 20:31:35 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	src_len;
 
 	i = 0;
-	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
+	if (!dst && size == 0)
+		return (src_len);
+	dst_len = ft_strlen(dst);
 	if (dst_len >= size)
 		return (size + src_len);
-	while (src[i] && i + dst_len < (size - 1))
+	while ((src[i] && i + dst_len < (size - 1)))
 	{
 		dst[dst_len + i] = src[i];
 		i++;
@@ -31,3 +33,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst[dst_len + i] = '\0';
 	return (dst_len + src_len);
 }
+
+// min{initial_dest), size}+ strlen(src).
+/*
+#include <stdio.h>
+#include <bsd/string.h>
+
+int main()
+{
+	char a[10] = "nyan !";
+	// char b[10] = "nyan !";
+
+	printf("mine: %ld\n", ft_strlcat(NULL, a, 0));
+	// printf("real: %ld\n", strlcat(NULL, b, 0));
+}
+*/
