a9245bb73a6ece21b78a294b4fca951a7f783dc2
Okan Emre KOCA <oknakoca@gmail.com>
2024-05-21 10:37:28 +0200

    cast received string from char* to unsigned char*, fixes bug

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 15:03:36 by okoca             #+#    #+#             */
-/*   Updated: 2024/05/21 10:10:30 by okoca            ###   ########.fr       */
+/*   Updated: 2024/05/21 10:36:52 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
-	char			*loc;
+	unsigned char	*loc;
 	unsigned char	cast;
 
 	i = 0;
-	loc = (char *)s;
+	loc = (unsigned char *)s;
 	cast = (unsigned char) c;
 	while (i < n)
 	{
